0a8886967e2324c9672254e7e4637c66832a0a52
daishihmr <daishi.hmr@gmail.com>
2014-07-15 23:03:29 +0900

    update

---
+++ libs/easing-min.js
@@ -0,0 +1,11 @@
+--2014-07-15 19:51:36--  https://raw.githubusercontent.com/danro/easing-js/master/easing-min.js
+Resolving raw.githubusercontent.com (raw.githubusercontent.com)... 103.245.222.133
+Connecting to raw.githubusercontent.com (raw.githubusercontent.com)|103.245.222.133|:443... connected.
+HTTP request sent, awaiting response... 200 OK
+Length: 3242 (3.2K) [text/plain]
+Saving to: 'easing-min.js'
+
+     0K ...                                                   100% 11.0M=0s
+
+2014-07-15 19:51:36 (11.0 MB/s) - 'easing-min.js' saved [3242/3242]
+
--- main.js
+++ main.js
@@ -2,9 +2,31 @@ var app;
 window.onload = function() {
     app = new Application();
 
-    new Rect(200, 100, 100)
+    var scene = app.currentScene;
+
+    new Rect(200, 50, 50)
         .setPosition(160, 160)
-        .addChildTo(app.currentScene)
-        .on("enterframe", function() {
+        .addChildTo(scene)
+        .glow();
+
+    new Rect(200, 40, 40)
+        .setPosition(160, 260)
+        .addChildTo(scene)
+        .glow();
+
+    new Fighter(200, 40, 40)
+        .setRotation(Math.PI * 0.5)
+        .setPosition(160, 60)
+        .addChildTo(scene)
+        .glow(undefined, 1.5)
+        .on("enterframe", function(e) {
+            var kb = e.params.app.keyboard;
+            var vector = kb.vector();
+            this.x += vector.x * 2;
+            this.y += vector.y * 2;
         });
+
+    var map = new Map([
+        [  1,  0,  -1,  0 ]
+    ]).addChildTo(scene);
 };
